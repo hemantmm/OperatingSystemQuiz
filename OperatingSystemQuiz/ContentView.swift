bb0d3d46dd852df3bfdbc3174f0e925d564ea55a
hemantmm <hemant29mehta@gmail.com>
2025-01-20 16:23:05 +0530

    TopicDetailView update

--- OperatingSystemQuiz/ContentView.swift
+++ OperatingSystemQuiz/ContentView.swift
@@ -10,11 +10,28 @@ import SwiftUI
 struct ContentView: View {
     
     
-    
     var body: some View {
         VStack {
            
             Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct TopicDetailView:View {
+    let topic:String
+    let description:String
+    
+    @Binding var currentView:String?
+    
+    var body: some View {
+        VStack {
+            Text(topic)
+                .padding()
+                .font(.headline)
+            Text(description)
+                .padding()
         }
         .padding()
     }
