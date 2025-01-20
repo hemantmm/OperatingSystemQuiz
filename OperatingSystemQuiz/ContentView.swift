c8ec9a227fd45fb865496d1e5a660934dc7b5dd8
hemantmm <hemant29mehta@gmail.com>
2025-01-20 16:30:48 +0530

    Questions updated

--- OperatingSystemQuiz/ContentView.swift
+++ OperatingSystemQuiz/ContentView.swift
@@ -37,6 +37,19 @@ struct TopicDetailView:View {
     }
 }
 
+struct QuizView: View {
+    @Binding var currnetView:String?
+    
+    @State private var questionIndex=0
+    @State private var score=0
+    @State private var showScoreAlert=false
+    
+    let questions=[
+        "What is Process?":["A program in execution","A stored file","A netwok request","A hardware device"],
+        "What is virtual memory?":["A memory managemant technique","A physical memory module","A storage disk","A network protocol"]
+    ]
+}
+
 #Preview {
     ContentView()
 }
